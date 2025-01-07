3769b98ac26119b8819bdb7919844ff0e440aeff
Gilvan-R-A <gilvan.dev@gmail.com>
2025-01-07 14:12:04 -0300

    feat: footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  standalone: true, 
+  imports: [RouterModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
