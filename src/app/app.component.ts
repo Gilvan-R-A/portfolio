28a3dc12f90bec61275b4fcbc9fe04b05c81d617
Gilvan-R-A <gilvan.dev@gmail.com>
2025-01-09 16:27:04 -0300

    feat: dark theme created and added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
   imports: [RouterModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   isMenuOpen = false;
+
+  constructor(private themeService: ThemeService) {}
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
+
+  get currentTheme(): 'dark' | 'light' {
+    return this.themeService.getCurrentTheme();
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
